53299bf311fefa11b58e625ab956c1d3fee108b0
Michael-Spisak <SpisakM21063@sus.htl-grieskirchen.at>
2025-12-10 14:17:57 +0100

    added more location drawing options

--- frontend/script.js
+++ frontend/script.js
@@ -3,28 +3,34 @@
 const backendUrl = "http://10.230.18.22:3000";
 
 const locations = [
-    { id: 0, x: 0, y: 185, xSize: 350, ySize: 165 },
-    { id: 1, x: 0, y: 515, xSize: 305, ySize: 90 },
-    { id: 2, x: 435, y: 0, xSize: 250, ySize: 110 },
-    { id: 3, x: 490, y: 365, xSize: 77, ySize: 95 },
-    { id: 4, x: 715, y: 175, xSize: 85, ySize: 215 }
-]
+    { id: 0, x: 0, y: 185, xSize: 350, ySize: 165, availableParts: 0, collectedParts: 0 },
+    { id: 1, x: 0, y: 515, xSize: 305, ySize: 90, availableParts: 0, collectedParts: 0 },
+    { id: 2, x: 435, y: 0, xSize: 250, ySize: 110, availableParts: 0, collectedParts: 0 },
+    { id: 3, x: 490, y: 365, xSize: 77, ySize: 95, availableParts: 0, collectedParts: 0 },
+    { id: 4, x: 715, y: 175, xSize: 85, ySize: 215, availableParts: 0, collectedParts: 0 }
+];
 
-const getStatus = async (backendUrl) => ((await fetch(`${backendUrl}/status`)).json());
-const getCollectedParts = async (backendUrl) => ((await fetch(`${backendUrl}/parts`)).json());
+const getStatus = async (backendUrl) => ((await fetch(`${backendUrl}`)).json());
+const getLocations = async (backendUrl) => ((await fetch(`${backendUrl}`)).json());
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const mapImage = new Image();
-mapImage.src = "map.png";
+mapImage.src = "../frontend/img/map.png";
 
 let currentStatus = [];
 let collectedParts = [];
 
+let currentLocationHovering = 0;
+
+getLocationsFromBackend();
+
 
 mapImage.onload = () => {
     drawImageInCanvas();
-    //testDoneLocations();
+    //testNotDoneLocations();
+    testDoneLocations();
+    //testAvailableLocations();
 }
 
 function drawImageInCanvas() {
@@ -33,16 +39,70 @@ function drawImageInCanvas() {
     console.log('drawImageInCanvas');
 }
 
-function drawDoneLocation(location) {
+function drawLocationDone(location) {
+    //ctx.clearRect(location.x, location.y, location.xSize, location.ySize);
     ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
     ctx.fillRect(location.x, location.y, location.xSize, location.ySize);
-    console.log(`location done and drawn at x=${location.x}, y=${location.y}, id=${location.id}`);
+    console.log(`location done and drawn at x = ${location.x}, y = ${location.y}, id = ${location.id}`);
 }
+
+function drawLocationNotDone(location) {
+    //ctx.clearRect(location.x, location.y, location.xSize, location.ySize);
+    ctx.fillStyle = "rgba(245, 189, 7, 0.5)";
+    ctx.fillRect(location.x, location.y, location.xSize, location.ySize);
+    //console.log(`location done and drawn at x = ${ location.x }, y = ${ location.y }, id = ${ location.id }`);
+}
+
+function drawLocationAvailable(location) {
+    //ctx.clearRect(location.x, location.y, location.xSize, location.ySize);
+    ctx.fillStyle = "rgba(133, 133, 133, 0.5)";
+    ctx.fillRect(location.x, location.y, location.xSize, location.ySize);
+    //console.log(`location done and drawn at x = ${ location.x }, y = ${ location.y }, id = ${ location.id }`);
+}
+
+// function drawNextLocation(location) {
+//     ctx.fillStyle = "rgba(0, 150, 255, 0.3)";
+// }
 
 
 function testDoneLocations() {
-    locations.forEach(location => drawDoneLocation(location));
+    locations.forEach(location => drawLocationDone(location));
 }
 
+function testNotDoneLocations() {
+    locations.forEach(location => drawLocationNotDone(location));
+}
+
+function testAvailableLocations() {
+    locations.forEach(location => {
+        if (location.collectedParts == 0)
+            drawLocationAvailable(location);
+    })
+}
+
+canvas.addEventListener('mousemove', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const xPos = e.clientX - rect.left;
+    const yPos = e.clientY - rect.top;
+
+    const hit = locations.find(
+        location => xPos >= location.x && xPos <= location.x + location.xSize &&
+            yPos >= location.y && yPos <= location.y + location.ySize
+    );
+
+    if (hit) {
+        currentLocationHovering = hit.id;
+        //console.log(`location with id: ${ currentLocationHovering } `);
+    }
+    else {
+        currentLocationHovering = -1;
+    }
+
+    //console.log(`last hovered location id: ${ currentLocationHovering } `);    
+});
 
 
+async function getLocationsFromBackend() {
+    let locationsBackend = await getLocations(`${backendUrl}/locations`);
+    console.log(locationsBackend);
+}
